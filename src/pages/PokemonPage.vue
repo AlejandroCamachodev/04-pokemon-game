d34437520011aa0d54302bb76f633cc38fe59efc
Alejandro <soloskilos@gmail.com>
2022-09-12 01:14:20 -0700

    Pokemon Game

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <h1>Quien es este pokemon?</h1>
+    <PokemonPicture :pokemonId="5" :showPokemon="true" />
+    <PokemonOptions/>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+
+export default {
+    components: { PokemonPicture, PokemonOptions }
+    
+}
+</script>
